bf8e25186263898fb015df2d988f718eb17972d8
wubabalubab <1277287760@qq.com>
2022-07-12 23:33:22 +0800

    Initial commit

---
+++ app/src/main/java/com/example/freemusic/InputDialog.java
@@ -0,0 +1,41 @@
+package com.example.freemusic;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+public class InputDialog extends DialogFragment {
+
+    EditText editText;
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        return super.onCreateDialog(savedInstanceState);
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.layout_fragment,container,false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        editText=view.findViewById(R.id.et_dialog);
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
